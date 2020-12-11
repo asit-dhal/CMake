178dcad7b1e7eee6fc16a015f9b873a86021759c
Asit Dhal <dhal.asitk@gmail.com>
2020-12-11 11:13:57 +0100

    wip: macos test config

--- Tests/RunCMake/install/DEFAULT-PERMISSIONS-TARGET_RUNTIME.cmake
+++ Tests/RunCMake/install/DEFAULT-PERMISSIONS-TARGET_RUNTIME.cmake
@@ -1,14 +1,14 @@
 enable_language(C)
 
-set(CMAKE_INSTALL_DEFAULT_TARGET_RUNTIME_PERMISSIONS OWNER_READ OWNER_EXECUTE)
+set(CMAKE_INSTALL_DEFAULT_TARGET_RUNTIME_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE)
 
 add_library(custom_perm_shared_lib SHARED obj1.c)
 set_target_properties(custom_perm_shared_lib PROPERTIES PREFIX "" SUFFIX "")
-install(TARGETS custom_perm_shared_lib LIBRARY PERMISSIONS OWNER_READ)
+install(TARGETS custom_perm_shared_lib LIBRARY PERMISSIONS OWNER_READ OWNER_WRITE)
 
 add_library(custom_perm_lib STATIC obj1.c)
 set_target_properties(custom_perm_lib PROPERTIES PREFIX "" SUFFIX "")
-install(TARGETS custom_perm_lib ARCHIVE PERMISSIONS OWNER_READ)
+install(TARGETS custom_perm_lib ARCHIVE PERMISSIONS OWNER_READ OWNER_WRITE)
 
 add_executable(default_perm_exe main.c)
 set_target_properties(default_perm_exe PROPERTIES PREFIX "" SUFFIX "")
@@ -16,4 +16,4 @@ install(TARGETS default_perm_exe RUNTIME)
 
 add_executable(custom_perm_exe main.c)
 set_target_properties(custom_perm_exe PROPERTIES PREFIX "" SUFFIX "")
-install(TARGETS custom_perm_exe RUNTIME PERMISSIONS OWNER_READ OWNER_EXECUTE WORLD_READ WORLD_EXECUTE)
+install(TARGETS custom_perm_exe RUNTIME PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE WORLD_READ WORLD_EXECUTE)
